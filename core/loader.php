1a43216f8f8e3c0472886806b74f853adb9c0024
Eduin A. May Uc <61023101+EduinMay12@users.noreply.github.com>
2022-01-10 15:43:15 -0600

    Delete core directory

--- core/loader.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-	//CARGAMOS LAS CONFIGURACIONES
-	require_once(_RAIZ.'core/config.php');
-
-	//CARGAMOS LIBRERIAS Y CLASES
-	require_once(_RAIZ.'include'.DIRECTORY_SEPARATOR.'funciones.php');	
-?>
